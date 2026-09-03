8863e47c354c2714b9f9ed3ac250fd9076cc977c
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-03 17:52:29 -0500

    Make c_pp_gnu_typeof a function macro

--- include/preprocessor/gnu/typeof.h
+++ include/preprocessor/gnu/typeof.h
@@ -4,11 +4,11 @@
 #include <preprocessor/stdc_version.h>
 
 #if defined(__GNUC__)
-    #define c_pp_gnu_typeof __typeof__
+    #define c_pp_gnu_typeof(...) __typeof__(__VA_ARGS__)
 #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= C_PP_STDC_VERSION_23)
-    #define c_pp_gnu_typeof typeof
+    #define c_pp_gnu_typeof(...) typeof(__VA_ARGS__)
 #else
-    #define c_pp_gnu_typeof
+    #define c_pp_gnu_typeof(...)
 #endif // #if defined(__GNUC__)
 
 #endif // #ifndef C_PREPROCESSOR_GNU_TYPEOF_H
